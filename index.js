b64849367f6bfa0450c7ddeed14c68ff51dc9a2d
Nikola <npopovic700@gmail.com>
2023-12-28 18:39:26 +0100

    Make js file working and fix ejs, made a css file in public dir

--- index.js
+++ index.js
@@ -8,45 +8,24 @@ const API_URL = "http://api.weatherstack.com";
 const API_KEY = "1884226f446d8337b2b4dbb3a54f35f9";
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {weather: null, error: null});
   });
 
 app.post("/", async (req, res) => {
   const city = req.body.city;
+  let newStr = city.replace(/ /g, "+");
   try {
-    const params = {
-      access_key: '1884226f446d8337b2b4dbb3a54f35f9',
-      query: city
-    }
-    const response = await axios.get(`http://api.weatherstack.com/current`, {params});
+    const response = await axios.get(`http://api.weatherstack.com/current?access_key=1884226f446d8337b2b4dbb3a54f35f9&query=${newStr}`);
     const result = response.data;
-    res.render("index.ejs", { weather: result.data });
+    res.render("index.ejs", { weather: result, error: null });
   } catch (error) {
-    res.render("index.ejs", {error: error.message});
+    res.render("index.ejs", {weather: null, error: error.message});
   }
 });
-
-
-
-
-// const params = {
-//   access_key: '1884226f446d8337b2b4dbb3a54f35f9',
-//   query: 'New York'
-// }
-
-// axios.get('https://api.weatherstack.com/current', {params})
-//   .then(response => {
-//     const apiResponse = response.data;
-//     console.log(`Current temperature in ${apiResponse.location.name} is ${apiResponse.current.temperature}℃`);
-//   }).catch(error => {
-//     console.log(error);
-//   });
-
-
-
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
